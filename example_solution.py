76cbd71b99810d2c48d34033dafc106409b8af1d
linuxseller <shibanutyitokom@yandex.ru>
2026-05-12 13:11:49 +0700

    Second task

--- example_solution.py
+++ example_solution.py
@@ -16,7 +16,7 @@ hahaton_easy_url = server_url+"/hahaton/easy/task/"
 url = hahaton_easy_url
 
 # first task
-def solve_task_1(data: str):
+def solve_task_1(data: str) -> str:
     task: dict = json.loads(data)
     scores: list[dict] = []
     for i in task['applicants']:
@@ -25,14 +25,59 @@ def solve_task_1(data: str):
         if i['project_experience']: score+=20
         if 20<=i['free_time']<40: score+=10
         if i['free_time']>=40: score+=20
-        scores.append({'name':i['name'], 'uuid':i['uuid'], 'score':score})
-    sorted_applicants = sorted(scores, key=itemgetter('score', 'uuid'))
+        scores.append({'uuid':i['uuid'],'name':i['name'],'score':score})
+    sorted_applicants = sorted(scores, key=itemgetter('score', 'uuid'))[:task["vacancies"]]
     return json.dumps({'best':sorted_applicants})
 
-task_url = url + '1'
+# "hh:mm" -> minutes since 00:00, i.e. clock2minutes("09:15") => 9*60+15=555
+def clock2minutes(time: str) -> int:
+    h, m = time.split(":")
+    return int(h)*60+int(m)
+
+def minutes2clock(minutes: int):
+    return str(minutes//60) + ":" + str(minutes%60)
+
+def solve_task_2(data: str) -> str:
+
+    def group_by_deadline(tasks: list[dict]) -> list[list[dict]]:
+        groups: list[list[dict]] = [[]]
+        tasks = sorted(tasks, key=lambda x: clock2minutes(x["deadline"]))
+        deadline_i = tasks[0]["deadline"]
+        groups_idx = 0
+        for task in tasks:
+            if task["deadline"] != deadline_i:
+                groups_idx += 1
+                groups.append([task])
+                deadline_i = tasks[groups_idx]["deadline"]
+            groups[groups_idx].append(task)
+        return groups
+
+    tasks:    list[dict] = json.loads(data)["tasks"]
+    schedule: list[int]  = []
+    time_now: int        = clock2minutes("09:00")
+    grouped_tasks: list[list[dict]] = group_by_deadline(tasks)
+
+    for group in grouped_tasks:
+        group_deadline: int = clock2minutes(group[0]["deadline"])
+        group = sorted(group,key=lambda x:x["duration"])
+        for task in group:
+            if time_now + task["duration"] > group_deadline:
+                break
+            # print(minutes2clock(time_now),"-",minutes2clock(time_now+task["duration"]), task["title"])
+            time_now += task["duration"]
+            schedule.append(task["id"])
+    return json.dumps({'schedule':schedule})
+
+task_url = url + 'MQ=='
 r = rq.get(task_url)
-print(r.text)
 solution = solve_task_1(r.text)
+payload = {'team':'FVS_bratva', 'solution':solution}
+r = rq.post(task_url, data=payload)
+print(r.text)
+
+task_url = url + 'Mg=='
+r = rq.get(task_url)
+solution = solve_task_2(r.text)
 payload = {'team':'FVS_bratva', 'solution':solution}
 r = rq.post(task_url, data=payload)
 print(r.text)
--- hahaton_tasks.py
+++ hahaton_tasks.py
@@ -1,29 +1,45 @@
 import json
 import random as rnd
 import uuid
-from operator import attrgetter
+from operator import itemgetter
 
 # task 1
 '''
-[
-  {
-    "name":<str>,
-    "uuid":<str>,
-    "git":<bool>,
-    "project_experience":<bool>,
-    "free_time":<int>
-  },
-  ...
-]
+task:
+{
+    "vacancies": <int>,
+    "applicants": [
+          {
+            "name":<str>,
+            "uuid":<str>,
+            "git":<bool>,
+            "project_experience":<bool>,
+            "free_time":<int>
+          },
+          ...
+      ]
+}
 наличие Git <=> 10 points
 опыт в проектах <=> 20 points
 сколько часов в неделю готов работать X <=>
     | X < 20      = 0
     | 20 <= X < 40 = 10 points
     | x >= 40     = 20 points
+
+response:
+{
+    "best": [
+        {
+            "name": <str>,
+            "uuid": <str>,
+            "score": <int>
+        }
+        ...
+    ]
+}
 '''
 task_1_top_n = 10
-def generate_task_1() -> json:
+def generate_task_1() -> str:
     rnd.seed(1)
     first_names: list[str] = [
         "James", "Olivia", "Liam", "Emma", "Noah",
@@ -50,8 +66,8 @@ def generate_task_1() -> json:
     task_condition: dict = {'vacancies':task_1_top_n, 'applicants':res}
     return json.dumps(task_condition)
 
-def solve_task_1() -> json.json:
-    task: dict = generate_task_1().loads()
+def solve_task_1() -> str:
+    task: dict = json.loads(generate_task_1())
     scores: list[dict] = []
     for i in task['applicants']:
         score = 0
@@ -59,30 +75,104 @@ def solve_task_1() -> json.json:
         if i['project_experience']: score+=20
         if 20<=i['free_time']<40: score+=10
         if i['free_time']>=40: score+=20
-        scores.append({'name':i[name], 'uuid':i['uuid'], 'score':score})
-    sorted_applicants = sorted(scores, key=attrgetter('score', 'uuid'))
+        scores.append({'name':i["name"], 'uuid':i['uuid'], 'score':score})
+    sorted_applicants = sorted(scores, key=itemgetter('score', 'uuid'))
     return json.dumps({'best':sorted_applicants})
 
 def verify_task_1(solution: str) -> bool:
     try: # if json passed is invalid, jic
         json_solution = json.loads(solution)['best']
+        print(task_1_top_n, len(json_solution))
         if task_1_top_n != len(json_solution):
-            return False
+            return False, "Some applicants should've come through"
     except:
-        return False
+        return False, "Incorrect json!"
     actual_solution = json.loads(solve_task_1())['best']
     count = 0 # amount of solution applicants contained in actual solution
-    for i in solution:
+    for i in json_solution:
         if i not in actual_solution:
-            return False
-    return True
+            print(i)
+            return False, "Wrong answer!"
+    return True, None
 
-def generate_task_2():
+# task 2
+
+'''
+WARNING: priority temporary is not considered and removed
+task:
+{
+    "tasks": [
+          {
+            "title": <str>,
+            "id": <int>,
+            "duration": <int>,
+            "deadline": <str: f"hh:mm">,
+            "priority": <int>
+          },
+          ...
+    ]
+}
+priority = range[1,20]
+priority 20 => very important
+priority 1 => not important
+
+Assume working day starts at 9:00 and ends at 19:00 without lunch break.
+
+response:
+{
+    "schedule": [<int>, ...]
+}
+'''
+
+def generate_task_2() -> str:
+    rnd.seed(1)
+    task_objective: list[str] = ["fix", "write", "review", "update", "meeting with devteam on"]
+    task_prjct: list[str] = ["main service", "program core", "new patch", "library module"]
+    minutes = ["00", "15", "30", "45"]
+    res: list[dict] = []
+    for i in range(25):
+        title: str = rnd.choice(task_objective) + ' ' + rnd.choice(task_prjct)
+        # generate random task duration such that possible times will be sums of two "minutes"
+        duration: int = sum(map(int, rnd.choices(minutes, k=2)))
+        deadline: str = str(rnd.randint(10, 19)) + ":" + rnd.choice(minutes)
+        # priority: int = rnd.randint(1,20)
+        res.append({
+            'title':title,
+            'id':i,
+            'duration':duration,
+            'deadline':deadline
+            # 'priority':priority
+            })
+    task_condition: dict = {'tasks':res}
+    return json.dumps(task_condition)
+
+## solve_task_2 is not needed due to several possible solutions
+def solve_task_2(): # unneded
     pass
-def solve_task_2():
-    pass
-def verify_task_2():
-    pass
+def verify_task_2(solution: str) -> (bool, str):
+    def clock2minutes(time: str) -> int:
+        h, m = time.split(":")
+        return int(h)*60+int(m)
+
+    try: # if json passed is invalid, jic
+        json_solution = json.loads(solution)['schedule']
+    except:
+        return False, "Incorrect json!"
+    task_cond = json.loads(generate_task_2())['tasks']
+    time_now = clock2minutes("09:00")
+    for i in json_solution:
+        curtask = task_cond[i]
+        if time_now + curtask["duration"] > clock2minutes(curtask["deadline"]):
+            return False, "Oh no, some deadline is missed!"
+        if time_now + curtask["duration"] > clock2minutes("19:00"):
+            return False, "Working day ended, you are late!"
+        time_now += curtask["duration"]
+    time_left = clock2minutes("19:00") - time_now
+    print(time_left)
+    for i in task_cond:
+        if time_left >= i["duration"] and i["id"] not in json_solution:
+            return False, "Could've done more tasks!"
+    return True, None
 
 
 def generate_task_3():
